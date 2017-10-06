3847416857707970b7eecb1daea3f3b7fb4d7b49
CONESTOGAC\mdrahorat4563 <michaldrahorat@gmail.com>
2017-10-06 12:16:55 -0400

    Added the unit test class library project

---
+++ Phase 2/Unit Tests/GameSnipeWebsite.Tests/GameSnipeWebsite.Tests/Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSnipeWebsite.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void AddGameShouldWork()
+        {
+            //Create the dummy game to test 
+            
+            //Game testGame = new Game(x, x, x)
+
+            //Insert the game using the defined function
+
+            //TODO: Add the addGame method call
+
+
+
+            Assert.Pass();
+        }
+    }
+}
