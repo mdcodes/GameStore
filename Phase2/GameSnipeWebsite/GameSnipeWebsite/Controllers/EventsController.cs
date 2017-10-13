5e7920b75b9682ddec358b08c5ff39528c050c6c
CONESTOGAC\mdrahorat4563 <michaldrahorat@gmail.com>
2017-10-13 13:32:19 -0400

    added more shit

---
+++ Phase2/GameSnipeWebsite/GameSnipeWebsite/Controllers/EventsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GameSnipeWebsite.Models;
+
+namespace GameSnipeWebsite.Controllers
+{
+    public class EventsController : Controller
+    {
+        private cvgsEntities1 db = new cvgsEntities1();
+
+        // GET: Events
+        public ActionResult Index()
+        {
+            var events = db.Events.Include(@ => @.Employee);
+            return View(events.ToList());
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            ViewBag.Employeeid = new SelectList(db.Employees, "id", "Name");
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,Date,Time,Location,Price,Employeeid")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(@event);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Employeeid = new SelectList(db.Employees, "id", "Name", @event.Employeeid);
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Employeeid = new SelectList(db.Employees, "id", "Name", @event.Employeeid);
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,Date,Time,Location,Price,Employeeid")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@event).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Employeeid = new SelectList(db.Employees, "id", "Name", @event.Employeeid);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event @event = db.Events.Find(id);
+            db.Events.Remove(@event);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Phase2/GameSnipeWebsite/GameSnipeWebsite/Controllers/GamesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GameSnipeWebsite.Models;
+
+namespace GameSnipeWebsite.Controllers
+{
+    public class GamesController : Controller
+    {
+        private cvgsEntities1 db = new cvgsEntities1();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            var games = db.Games.Include(g => g.Category).Include(g => g.Developer).Include(g => g.Employee);
+            return View(games.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            ViewBag.Categoryid = new SelectList(db.Categories, "id", "Name");
+            ViewBag.Developerid = new SelectList(db.Developers, "id", "Name");
+            ViewBag.Employeeid = new SelectList(db.Employees, "id", "Name");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,Price,Employeeid,Developerid,Categoryid")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Categoryid = new SelectList(db.Categories, "id", "Name", game.Categoryid);
+            ViewBag.Developerid = new SelectList(db.Developers, "id", "Name", game.Developerid);
+            ViewBag.Employeeid = new SelectList(db.Employees, "id", "Name", game.Employeeid);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Categoryid = new SelectList(db.Categories, "id", "Name", game.Categoryid);
+            ViewBag.Developerid = new SelectList(db.Developers, "id", "Name", game.Developerid);
+            ViewBag.Employeeid = new SelectList(db.Employees, "id", "Name", game.Employeeid);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,Price,Employeeid,Developerid,Categoryid")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categoryid = new SelectList(db.Categories, "id", "Name", game.Categoryid);
+            ViewBag.Developerid = new SelectList(db.Developers, "id", "Name", game.Developerid);
+            ViewBag.Employeeid = new SelectList(db.Employees, "id", "Name", game.Employeeid);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
